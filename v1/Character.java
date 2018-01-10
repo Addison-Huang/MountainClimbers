f08a3e2bddb734cf73a57a8e07a0c6067ea33b9c
simont1 <stsui2@stuy.edu>
2018-01-10 08:38:43 -0500

    jan 10

--- v1/Character.java
+++ v1/Character.java
@@ -6,4 +6,42 @@ public class Character {
     private int lossCounter;
     private String[] shop;
     private String[] inventory;
+    
+    public Character(){
+	username = "guest";
+	password = "password";
+	balance = 1000;
+	shop =  new String[10];  // ["ring", "", "", "", "", "", "", "", "", ""];
+    }
+    public Character(String newName, String newPassword){
+	this();
+	username = newName;
+	password = newPassword;
+    }
+    public void choosePlace(){
+	System.out.println("Choose a place to go: ");
+	String location = "casino";  //takes in input from person
+	if(location.equals("Casino")){
+	    chooseGame();
+	}
+	else if(location.equals("Bar")){
+	    chooseDrink();
+	}
+	    
+    }
+    public void chooseGame(){
+	System.out.println("Choose a game:");
+	String s = "slots"; //prompts user
+	if(s.equals("slots")){
+
+	}
+	else if(s.equals("roulette")){
+
+	}
+	
+	
+    }
+    public void chooseDrink(){
+
+    }
 }
--- v1/Woo.java
+++ v1/Woo.java
@@ -1,5 +1,6 @@
 public class Woo{
     public static void main(String[] args){
-
+	Character player = new Character();
+	//Game newGame = new blackjack()
     }
 }
