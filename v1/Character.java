7a702c0b8168de4f7b9a06651a8d30b36c0803ef
Kenny <lli15@stuy.edu>
2018-01-10 08:16:49 -0500

    Character.java

---
+++ v1/Character.java
@@ -0,0 +1,9 @@
+public class Character {
+    private String username;
+    private String password;
+    private double balance;
+    private int winCounter;
+    private int lossCounter;
+    private String[] shop;
+    private String[] inventory;
+}
