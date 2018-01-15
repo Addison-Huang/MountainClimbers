d0c702c60acec68ee771d43efb2f2e0f9bb5ba48
Addison <Addison@Ryans-MBP.home>
2018-01-14 23:05:04 -0500

    Baccarat

---
+++ v2/Baccarat.java
@@ -0,0 +1,12 @@
+public class Baccarat extends Game {
+    public Baccarat() {
+	name = "Baccarat";
+    }
+
+    protected boolean playOnce(Character player) {
+    }
+
+    protected boolean outcome(Character player) {
+    }
+    
+}
