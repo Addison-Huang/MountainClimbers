590178cb3bf561eeab29dce8c90335bf59969e41
Addison <Addison@Ryans-MBP.home>
2018-01-15 22:09:37 -0500

    Slots commented

--- v5/Slots.java
+++ v5/Slots.java
@@ -1,5 +1,6 @@
 import cs1.Keyboard;
 public class Slots extends Game {
+    //instance var storing all combinations
     private final String[] combinations  = { 
 	"lemon", "lemon", "lemon", 
 	"seven", "seven", "seven", 
@@ -10,9 +11,11 @@ public class Slots extends Game {
 	"star", "star", "star",
 	"banana", "banana", "banana"
     };
-  
+
+    //mutable instance var
     private String[] _combinations;
 
+    //default constructor
     public Slots() {
 	name = "Slots";
 	_combinations = new String[combinations.length ];
@@ -21,24 +24,27 @@ public class Slots extends Game {
 	}
     }
 
+    //returns the first 3 items of combinations
     public String display() {
 	return _combinations[0] + " | " +
 	    _combinations[1] + " | " +
 	    _combinations[2];
     }
 
+    //swaps the value at index i with the value at index j
     private void swap( int i, int j ) {
 	String foo = _combinations[i];
 	_combinations[i] = _combinations[j];
 	_combinations[j] = foo;
     }
 
+    //spins the slot
     public void spinOnce() {
 	for( int i = 0; i < _combinations.length; i++ )
 	    swap( i, (int)( Math.random() * _combinations.length) );
     }
 
-
+    //determines how much the winnings were
     public double whatWin() {
 	if (_combinations[0].equals("seven")) {
 	    winnings = 1000.0;
@@ -50,6 +56,7 @@ public class Slots extends Game {
 	}		
     }
 
+    //overwritten toString with the instructions
     public String toString() {
 	String retStr = "Welcome to Slots! Here are the rules.\n";
 	retStr += "Spinning the slots machine costs $20.\n";
@@ -57,7 +64,8 @@ public class Slots extends Game {
 	retStr += "Anything else, you lose.\n";
 	return retStr;
     }
-	    
+
+    //plays the game 
     protected void playOnce(Character player) {
 	System.out.println(this);
 	System.out.println("Your current balance is $" + player.getBal());	
@@ -72,6 +80,7 @@ public class Slots extends Game {
 	}
     }
 
+    //checks if it was a win or a loss
     protected void outcome(Character player) {
 	if ( _combinations[0].equals(_combinations[1]) && _combinations[0].equals(_combinations[2])) {
 	    System.out.println("Congratulations! you won $" + whatWin());
