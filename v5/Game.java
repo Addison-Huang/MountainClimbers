61af5d13a446e471e312277f95315bd6293b0cbf
Addison <Addison@Ryans-MBP.home>
2018-01-15 22:05:52 -0500

    commented game

--- v5/Game.java
+++ v5/Game.java
@@ -1,18 +1,22 @@
 import cs1.Keyboard;
 abstract class Game {
+    //instances variables
     protected String name;
     protected double winnings;
 
-	protected boolean toContinue() {
+    //method in which it asks the player to continue
+    protected boolean toContinue() {
 	System.out.println("Continue? (yes or no)");
 	String cont = Keyboard.readString();
 	if (cont.equals("yes")) {
-		return true;
+	    return true;
 	} else {
-		return false;
+	    return false;
 	}
-	}
+    }
+    //simulates the game, runs it
     protected abstract void playOnce(Character player);
+    //checks whether it was a win or loss
     protected abstract void outcome(Character player);
     
 }
